f34f3dc3aacf6f6e56ad32968a4ca53cba033f83
FrancoAdN <francoadinapoli@gmail.com>
2020-06-12 11:02:46 -0300

    add

--- page/src/App.js
+++ page/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
