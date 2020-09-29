0c1c9a1662cc1b38377c1a1dbc1b118d79c15a1c
FrancoAdN <francoadinapoli@gmail.com>
2020-09-29 10:22:31 -0300

    'add'

--- server page max/schema.js
+++ server page max/schema.js
@@ -417,6 +417,16 @@ const RootQuery = new GraphQLObjectType({
                 const sql = `SELECT * FROM Empresas e, Contactos_empresa contemp, Cotizaciones c, Estados_coti ec, Detalle_coti dc, Empleados emp WHERE e.id_empresa=contemp.id_empresa AND c.id_empresa=e.id_empresa AND c.orden_coti=ec.orden_coti AND ec.orden_coti=dc.orden_coti AND emp.id_empleado=c.id_empleado;`
                 return get_empresas(await query_third_db(sql))
             }
+        },
+        empresas_por_id: {
+            type: new GraphQLList(EmpresaType),
+            args: {
+                id_empresa: { type: GraphQLID }
+            },
+            resolve: async (parent, args) => {
+                const sql = `SELECT * FROM Empresas e, Contactos_empresa contemp, Cotizaciones c, Estados_coti ec, Detalle_coti dc, Empleados emp WHERE e.id_empresa=contemp.id_empresa AND c.id_empresa=e.id_empresa AND c.orden_coti=ec.orden_coti AND ec.orden_coti=dc.orden_coti AND emp.id_empleado=c.id_empleado AND emp.id_empresa=${args.id_empresa};`
+                return get_empresas(await query_third_db(sql))
+            }
         }
 
 
