5d32867102deb6ebfcb3214b33774d6a092ba2bd
FrancoAdN <francoadinapoli@gmail.com>
2020-10-09 01:50:47 -0300

    add

--- server page max/api.js
+++ server page max/api.js
@@ -141,7 +141,7 @@ app.post('/login_contact', async (req, resp) => {
 
 })
 
-app.get('/ticket', (req, resp) => {
+app.get('/ticket', async (req, resp) => {
     const sql = 'select * from Ticket t, Respuesta_ticket rt WHERE t.Id_ticket = rt.Id_ticket and t.Id_empresa = 1'
     resp.send(await query_third_db(sql))
 })
